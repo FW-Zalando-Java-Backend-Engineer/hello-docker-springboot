81fd3783082f6cd8160e370c60087d775d097071
safwan-kher <eng.safwan.kherallah@gmail.com>
2025-07-16 11:46:30 +0200

    init commit

---
+++ src/main/java/org/example/hellodockerspringboot/HelloDockerSpringbootApplication.java
@@ -0,0 +1,13 @@
+package org.example.hellodockerspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloDockerSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloDockerSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/hellodockerspringboot/HelloDockerSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hellodockerspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloDockerSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
